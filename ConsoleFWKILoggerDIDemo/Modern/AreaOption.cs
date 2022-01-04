a6a1a697105ce0d181a542d8d7695a0e2e0a7855
Joy George Kunjikkuru <joymon@gmail.com>
2022-01-03 20:02:06 -0500

    Added eventlog

--- ConsoleFWKILoggerDIDemo/Modern/AreaOption.cs
+++ ConsoleFWKILoggerDIDemo/Modern/AreaOption.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.EventLog;
 
 namespace ConsoleFWKILoggerDIDemo.Modern
 {
